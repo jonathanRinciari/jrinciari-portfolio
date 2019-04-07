5ac2c36902d1a5397ccc7687cb909cda93e96214
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-04-07 14:24:58 -0400

    feat: Setup about section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './pages/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
