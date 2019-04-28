60b615c06de7cb2f73e2b3c790550e6a3ce5ae4f
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-04-28 10:39:08 -0400

    added collapselogic

--- src/app/pages/header/header/header.component.ts
+++ src/app/pages/header/header/header.component.ts
@@ -8,13 +8,22 @@ import { Component, OnInit, HostListener, Output, EventEmitter, Input } from '@a
 export class HeaderComponent implements OnInit {
   @Output() scrollTo = new EventEmitter();
   @Input() collapse = false;
+  navbarClicked = false;
   constructor() { }
 
   ngOnInit() {
   }
 
   onClick(val) {
+    if (this.navbarClicked) {
+      this.navbarClicked = false;
+    }
+
     this.scrollTo.emit(val);
+  }
+
+  handleOnClick() {
+    this.navbarClicked = !this.navbarClicked;
   }
 
 }
