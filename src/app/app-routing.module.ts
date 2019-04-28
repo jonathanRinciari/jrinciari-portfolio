929242204a190deca5f036ae4078b610c1695f2e
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-04-28 19:34:46 -0400

    added routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { WritingOverviewComponent } from './pages/writing-overview/writing-overview.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: MainComponent },
+  { path: 'writing', component: WritingOverviewComponent },
+  { path: '**', redirectTo: 'home'}
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule {}
