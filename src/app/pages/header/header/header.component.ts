cd225551d2078efc4016fa488e7cf15a7ddecea8
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-05-02 19:47:47 -0400

    removed unused

--- src/app/pages/header/header/header.component.ts
+++ src/app/pages/header/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
