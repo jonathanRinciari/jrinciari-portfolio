83f6029d0a18c11782b19743c9f678e53e7a3ddb
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-05-02 19:47:26 -0400

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,4 @@ import { Component, HostListener } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-}
+export class AppComponent {}
