585fcb351d14c259ed6c781890adaf09c4f08ee2
Jonathan Rinciari <jrinciari@greencheckverified.com>
2019-05-11 13:40:56 -0400

    turned comment into TODO to find easier

--- src/app/pages/writing-overview/writing-overview.component.ts
+++ src/app/pages/writing-overview/writing-overview.component.ts
@@ -13,6 +13,7 @@ export class WritingOverviewComponent implements OnInit {
   }
 
   /**
+   * // TODO
    * This section is a work in Progress
    * It needs to display all blog posts using the card on main
    * When clicked it will move to a new page to display the blog
